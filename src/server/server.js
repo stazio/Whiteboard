9c5d5248194d9903fc9d6b377e7b654ead7f10c9
Staz.IO <staz@staz.io>
2017-12-29 15:44:58 -0500

    Added tests!!!!!!!!!

--- src/server/server.js
+++ src/server/server.js
@@ -1,67 +1,25 @@
-var fs = require('fs');
-var WebSocket = require('ws');
-var config = require('config');
+let fs = require('fs');
+let WebSocket = require('ws');
+let config = require('config');
+let winston = require('winston');
 
-var ws = create_server(1234, config.get('ssl.enabled'), config.get('ssl.privkey'), config.get('ssl.certificate'));
+let ws, room, webserver;
+let nextPathID = 1;
 
-if (fs.existsSync("config/storage.json"))
-    var room = JSON.parse(fs.readFileSync("config/storage.json"));
-else
-    room = {};
+if (require.main === module) {
+    winston.info("I see you are running me directly! I guess I better turn on eh?");
+    initialize();
+}else {
+    module.exports = {
+        containerId: "server",
+        initialize: initialize
+    }
+}
 
-var nextPathID = 1;
-
-ws.on('connection', function (client, request) {
-    client.request = request;
-
-    client.send(JSON.stringify({
-        paths: room,
-        type: "room"
-    }));
-
-    console.log("[" + client.request.connection.remoteAddress + "]: joined");
-
-    client.on('message', function (text) {
-        var json = JSON.parse(text);
-        for (var i in json) {
-            if (json.hasOwnProperty(i)) {
-                var data = json[i];
-                switch (data.type) {
-                    case "path":
-                        if (data.path)
-                            process_path(client, data.path);
-                        if (data.paths) {
-                            for (var ii in data.paths) {
-                                if (data.paths.hasOwnProperty(ii))
-                                    process_path(client, data.paths[ii]);
-                            }
-                        }
-                        break;
-                    case "clear":
-                        broadcast({"type": "clear"});
-                        room = {};
-                        break;
-                }
-            }
-        }
-    });
-
-    client.on('close', function () {
-        console.log("[" + client.request.connection.remoteAddress + "]: left");
-    });
-
-    client.on('error', function (errno, code) {
-        console.log("[" + client.request.connection.remoteAddress + "]: has had an error. " + "Error Code: " + code);
-    });
-
-    client.on('pong', function () {
-        client.isAlive = true
-    });
-});
 
 function clientErr(client, error) {
     if (error)
-        console.log("[" + client.request.connection.remoteAddress + "]: had an error while sending a message! Error: " + error);
+        winston.info("[" + client.request.connection.remoteAddress + "]: had an error while sending a message! Error: " + error);
 }
 
 function process_path(client, path) {
@@ -69,16 +27,19 @@ function process_path(client, path) {
         if (room[client.activePathID]) {
             room[client.activePathID].push(path);
             sendExcept(client, client.activePathID, path);
+            client.send(JSON.stringify({'type' : 'path_response'}));
         }
         client.activePathID = null;
     } else if (typeof path === "string") {
         client.activePathID = nextPathID++;
         room[client.activePathID] = [path];
         sendExcept(client, client.activePathID, path);
+        client.send(JSON.stringify({'type' : 'path_response', 'path_id' : client.activePathID}));
     } else {
         if (room[client.activePathID]) {
             room[client.activePathID].push(path);
             sendExcept(client, client.activePathID, path);
+            client.send(JSON.stringify({'type' : 'path_response', 'path_id' : client.activePathID}));
         }
     }
 }
@@ -106,23 +67,94 @@ function sendExcept(clientt, pathID, path) {
  * @returns ws.Server
  */
 function create_server(port, ssl, privkey, certificate) {
-    var server;
     if (ssl) {
-        console.log("Starting the server using SSL!");
-        console.log("Privilege Key Path" + privkey);
-        console.log("Certificate Key Path" + certificate);
-        server = require('https').createServer({
+        winston.info("Starting the server using SSL!");
+        winston.info("Privilege Key Path" + privkey);
+        winston.info("Certificate Key Path" + certificate);
+        webserver = require('https').createServer({
             key: fs.readFileSync(privkey),
             cert: fs.readFileSync(certificate)
         });
-        server.listen(port);
+        webserver = require('http-shutdown')(webserver);
+        webserver.listen(port);
     } else {
-        console.log("Starting the server without SSL! You might wanna fix that maybe?");
-        server = require('http').createServer();
-        server.listen(port);
+        winston.info("Starting the server without SSL! You might wanna fix that maybe?");
+        webserver = require('http').createServer();
+        webserver = require('http-shutdown')(webserver);
+        webserver.listen(port);
     }
 
-    return new WebSocket.Server({server: server});
+    return new WebSocket.Server({server: webserver});
+}
+
+function initialize(port, logLevel) {
+    if (logLevel !== undefined)
+    winston.level = logLevel;
+    ws = create_server(port ? port : 1234, config.get('ssl.enabled'), config.get('ssl.privkey'), config.get('ssl.certificate'));
+
+    if (fs.existsSync("config/storage.json"))
+        room = JSON.parse(fs.readFileSync("config/storage.json"));
+    else
+        room = {};
+
+    ws.on('connection', function (client, request) {
+        client.request = request;
+
+        client.send(JSON.stringify({
+            paths: room,
+            type: "room"
+        }));
+
+        winston.info("[" + client.request.connection.remoteAddress + "]: joined");
+
+        client.on('message', function (text) {
+            var json = JSON.parse(text);
+            if (!Array.isArray(json))
+                json = [json];
+            for (var i in json) {
+                if (json.hasOwnProperty(i)) {
+                    var data = json[i];
+                    switch (data.type) {
+                        case "path":
+                            if (data.path)
+                                process_path(client, data.path);
+                            if (data.paths) {
+                                for (var ii in data.paths) {
+                                    if (data.paths.hasOwnProperty(ii))
+                                        process_path(client, data.paths[ii]);
+                                }
+                            }
+                            break;
+
+                        case "clear":
+                            broadcast({"type": "clear"});
+                            room = {};
+                            break;
+
+                        case "request_room":
+                            winston.info("Sending!");
+                            client.send(JSON.stringify({
+                                "type" : "room",
+                            paths: room}));
+                            break;
+                    }
+                }
+            }
+        });
+
+        client.on('close', function () {
+            winston.info("[" + client.request.connection.remoteAddress + "]: left");
+        });
+
+        client.on('error', function (errno, code) {
+            winston.info("[" + client.request.connection.remoteAddress + "]: has had an error. " + "Error Code: " + code);
+        });
+
+        client.on('pong', function () {
+            client.isAlive = true
+        });
+    });
+
 }
 
 function broadcast(data) {
@@ -138,7 +170,7 @@ function broadcast(data) {
 setInterval(function () {
     ws.clients.forEach(function (conn) {
         if (conn.isAlive === false) {
-            console.log("[" + conn.request.connection.remoteAddress + "]: timed out");
+            winston.info("[" + conn.request.connection.remoteAddress + "]: timed out");
             conn.terminate();
         }
 
@@ -150,6 +182,6 @@ setInterval(function () {
 setInterval(function () {
     fs.writeFile("config/storage.json", JSON.stringify(room), function (err) {
         if (err)
-            console.log("Failed to save room state! Error: " + err);
+            winston.info("Failed to save room state! Error: " + err);
     });
 }, 10 * 1000);
---
+++ src/server/test/test.js
@@ -0,0 +1,101 @@
+let expect = require('chai').expect;
+let WebSocket = require('ws');
+let server = require('../server');
+let fs = require('fs');
+let ws;
+
+beforeEach(function() {
+    let port = Math.floor(1000 + (Math.random() * 1000));
+    server.initialize(port, 0);
+    ws = new WebSocket("ws://localhost:" + port);
+});
+
+it ('connect', function(done){
+    ws.on('open', done);
+});
+
+it ('compare room message', function(done) {
+    ws.once('message', function(data) {
+        var json = JSON.parse(data);
+        expect(json).to.be.an('object').and.to.have.property('type');
+        expect(json.type).to.equal('room');
+        expect(json.paths).to.deep.equal(JSON.parse(fs.readFileSync('config/storage.json', 'utf-8')));
+        done();
+    });
+});
+
+describe('Once Connected', function() {
+    beforeEach(function(done) {
+        ws.once('message', function(data){
+            let json = JSON.parse(data);
+            expect(json).to.be.an('object').and.to.have.property('type');
+            expect(json.type).to.equal('room');
+            expect(json.paths).to.deep.equal(JSON.parse(fs.readFileSync('config/storage.json', 'utf-8')));
+            done();
+        });
+    });
+
+    it ('request new room', function(done) {
+        ws.on('message', function(data) {
+            let json=  JSON.parse(data);
+            expect(json).to.be.an('object').and.to.have.property('type');
+            if (json.type === 'room') {
+                expect(json.type).to.equal('room');
+                expect(json.paths).to.deep.equal(JSON.parse(fs.readFileSync('config/storage.json', 'utf-8')));
+                done();
+            }
+        });
+        ws.send(JSON.stringify({
+            "type" : "request_room"
+        }));
+    });
+
+    it ('send a new path, check new room', function(done) {
+        ws.send(JSON.stringify({
+            "type" : "path",
+            "path" : "black;1"
+        }));
+        ws.once('message', function(data) {
+            let json=  JSON.parse(data);
+            expect(json).to.be.an('object').and.to.have.property('type');
+            expect(json.type).to.equal('path_response');
+            expect(json).to.have.property('path_id');
+            let fileData = JSON.parse(fs.readFileSync('config/storage.json', 'utf-8'));
+            fileData[json.path_id] = ["black;1"];
+
+            ws.send(JSON.stringify({
+                "type" : "request_room"
+            }));
+            ws.once('message', function(data) {
+                json=  JSON.parse(data);
+                expect(json).to.be.an('object').and.to.have.property('type');
+                expect(json.type).to.equal('room');
+                expect(json.paths).to.deep.equal(fileData);
+                done();
+            });
+
+        });
+    });
+
+    it ('clear the room', function() {
+        ws.send(JSON.stringify({
+            "type" : "clear"
+        }));
+        ws.once('message', function(data) {
+            let json = JSON.parse(data);
+            expect(json).to.be.an('object').and.to.have.property('type');
+            expect(json.type).to.equal('clear');
+
+            ws.send(JSON.stringify({
+                "type" : "request_room"
+            }));
+            ws.once('message', function(data) {
+                json=  JSON.parse(data);
+                expect(json).to.be.an('object').and.to.have.property('type');
+                expect(json.type).to.equal('room');
+                expect(json.paths).to.deep.equal({});
+                done();
+            });
+        });
+    })
+});
